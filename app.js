952b83e7947e232d27a3936c9e942085e5bb8bd1
omar <onarmohanad@gmail.com>
2024-06-04 16:46:43 +0300

    updet file

--- app.js
+++ app.js
@@ -2,20 +2,31 @@
 
 // let gender = prompt ("Enter your gender");
 
+// while(gender != "male" && gender != "female"){
+//     prompt ("Enter your gender");
+
+// }
+
 // if (gender == "male"){
 //     alert("Welcome sir " + name);
 // }
 // else if (gender == "female"){
 //     alert("welcome mrs " + name);
 // }
-// else{ 
-//      alert("welcome" + name);
-// }
+
 
 // let order = prompt ("Dount or Coffee or Ice cream or Bakery");
+// let ordername = prompt (" Snow Dount or Mocha or vanilla or Bakery");
 // alert ("their order is getting prepared");
 // console.log(name + order);
 
+// let myArray = [name, gender, order, ordername]
+
+// for(let i = 0; i< myArray.length; i++){
+//     console.log(myArray[i]);
+// }
+
+/*q1 */
 
 // console.log(-5*3);
 // console.log("JavaScript" + 50);
@@ -30,10 +41,12 @@
 // console.log( Math.pow(1,2));
 
 /*q2 */
+
 // let num = prompt ("Enter your num");
 // alert(num);
 
 /*q3*/
+
 // let num1 = prompt ("Enter your num");
 // let num2 = prompt ("Enter your num");
 
@@ -46,6 +59,7 @@
 // }
 
 /*q4*/
+
 // let num3 = prompt ("Enter your num");
 // let num4 = prompt ("Enter your num");
 
@@ -64,7 +78,11 @@
 // let num2 =Number( prompt ("Enter your num"));
 // alert( sum = num1 + num2)
 
-// let Number = 1;
+
+/*q6 */
+
+
+// let Number = 4;
 // switch(Number) {
 //     case 1:
 //         console.log("ONE");
@@ -96,3 +114,46 @@
 //     default:
 //         console.log("PLEASE TRY AGAIN");
 // }
+
+
+
+/*q7 */
+
+// for(let i =0; i<=5; i++){
+//     alert(i);
+// }
+
+/*q8 */
+// let q8 =[];
+// for(let i =0; i<=5; i++){
+//     q8.push(i);
+//     alert(q8);
+// }
+
+
+/*q9 */
+// let q9 =[]
+// for(let i = 0; i<= 20; i++){
+//     if(i % 3 ===0){
+//         q9.push(i)
+//     }
+// }
+// alert(q9);
+
+/*q10*/
+
+// let Number1 = prompt("Enter number");
+// while(Number1 >100 || Number1< 0){
+//     alert("Error")
+//     Number1 = prompt("Enter number")
+// }
+// alert("god job")
+
+/*q11 */
+
+// let Number2 = prompt("Enter number");
+// let sum = 0;
+// for (let i =0; i<=Number2; i++){
+//     sum = sum + i;
+// }
+// alert(sum)
